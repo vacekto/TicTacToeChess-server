3e200ba839a1b1ad28dd8e76272e4e43df6653d7
Tomas Vacek <vacekto@gmail.com>
2023-06-09 08:42:11 +0200

    fixing deploy bug

--- src/copyClientScript.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require('path');
-
-fs.cpSync(
-    path.join(__dirname, 'client', 'build'),
-    path.join(__dirname, '/../dist/client/build'),
-    { recursive: true }
-);
-
